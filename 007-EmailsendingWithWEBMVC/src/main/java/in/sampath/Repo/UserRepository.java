4957acb4e03e90a72dd8e3a73ebca9de5df512f7
sampathkatkam <sampathkatkam785@gmail.com>
2024-05-19 20:07:08 +0530

    deleted

--- 007-EmailsendingWithWEBMVC/src/main/java/in/sampath/Repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package in.sampath.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.sampath.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByEmail(String email);
-}
